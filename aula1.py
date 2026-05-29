9f942f5b47c8719976467ab7ddeba5a20a8a4733
Jonathan Alexandre Rodrigues Alves <91800109+Jhon2910@users.noreply.github.com>
2026-05-29 10:23:12 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -1,11 +1,18 @@
+# comentario por linha
+"""
+comentario multilinhas
+
+"""
+
 print("Hello World") # print
+
 
 primeiroNumero = 10
 segundoNumero = 5
 multiplicar = primeiroNumero * segundoNumero
 
 print("Resultado: ", multiplicar)
-
+#----------------------------------------------
 media = 0
 
 primeiraNota = float(input("Digite a primeira nota: ")) # primeira nota vira float
@@ -18,6 +25,75 @@ media += terceiraNota
 media /= 3.0
 
 print ("media = ", media)
+#--------------------------------
 
+frase = "sistemas de informação"
+
+upperfrase = frase.upper()# aumentar todas as letras
+lowerfrase = frase.lower()# diminuir
+dividido = frase.split()#espacos em branco
+
+print(upperfrase)
+print(lowerfrase)
+print(dividido)
+
+#----------------------------
+# condicional
+
+tempo = int(input("Quantos anos o computador tem? : "))
+
+if tempo <= 3:
+    print("Computador novo")
+
+else:
+    print("Computador velho")
+
+print("Fim")
+
+#elif // in
+
+time = input("Qual seu time?")
+if time == "Atletico":
+    print("Boa escolha")
+
+elif time == "Flamengo" or time == "Palmeiras" or time == "corinthians":
+    print("Seu time e popular")
+
+elif time in "Gremio internacional Juventude": # in = algum dentro da string
+    print("Seu time e do RS")
+
+else:
+    print("Okay")
+
+#----------------------
+
+#repeticao
+
+
+#for
+
+for contador in range(0, 50):
+    print("oi")
+
+for contador in range(0, 50, 2): # 2 em 2
+    print(contador)
+
+for contador in range(50, 0, -1): # regressivo
+    print(contador)
+
+#while
+
+r = "s"
+while r == "s":
+    n = int(input("Digite o valor"))
+    r = str(input("Quer continuar? [s/n] "))
+    r = r.upper()
+print("Fim")
+
+#-------------------------
+
+#lista
+
+lista = [1,"a" , ["ab, cd"]]
 
 
