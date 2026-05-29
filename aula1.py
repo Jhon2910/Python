4969f3b74e9a3a2660b2a02507c8ef8169cac86e
Jonathan Alexandre Rodrigues Alves <91800109+Jhon2910@users.noreply.github.com>
2026-05-29 09:39:48 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,23 @@
+print("Hello World") # print
+
+primeiroNumero = 10
+segundoNumero = 5
+multiplicar = primeiroNumero * segundoNumero
+
+print("Resultado: ", multiplicar)
+
+media = 0
+
+primeiraNota = float(input("Digite a primeira nota: ")) # primeira nota vira float
+media += primeiraNota
+segundoNota = float(input("Digite a segunda nota: "))
+media += segundoNota
+terceiraNota = float(input("Digite a terceira nota: "))
+media += terceiraNota
+
+media /= 3.0
+
+print ("media = ", media)
+
+
+
