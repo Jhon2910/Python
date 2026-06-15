8be2e0245708e15e59be01cb6f0f6dd9e385ae4c
Jonathan Alexandre Rodrigues Alves <91800109+Jhon2910@users.noreply.github.com>
2026-06-14 21:22:49 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,20 +1,41 @@
 from socket import *
+import random
+
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
-nome = input('Digite seu nome: ')
-clientSocket.send(nome.encode())
+clientSocket.connect((serverName, serverPort))
 
-BoasVindas = clientSocket.recv(1024)
-print ('Olá', BoasVindas.decode())
+while True:
 
-Nivel = input('Digite sua Nivel: ')
-clientSocket.send(Nivel.encode())
+    nome = input('\nDigite seu nome: ')
+    clientSocket.send(nome.encode())
+    clientSocket.recv(1024).decode()
+    print('Olá', nome)
 
-Jogo = input('\nQual jogo gostaria de jogar? ')
-clientSocket.send(Jogo.encode())
+    nivel = input('Digite sua nivel (1 a 100): ')
+    clientSocket.send(nivel.encode())
+    clientSocket.recv(1024).decode()
+    print("Seu nivel é: ", nivel)
 
+    Jogo = input('\nQual jogo gostaria de jogar? ')
+    clientSocket.send(Jogo.encode())
+    clientSocket.recv(1024).decode()
 
+    jogadores = int(clientSocket.recv(1024).decode())
 
-clientSocket.close()
+    if Jogo == 'Valorant':
+        necessarios = 4 - jogadores
+        players = [jogadores]
+        if necessarios > 0:
+            print("Sao necessarios mais ", necessarios, " para jogar, você foi adicionado a fila de valorant.")
+        else:
+            ID = random.randint(1, 300)
+            print("Quantidade de jogadores alcançada!: ", jogadores)
+            print("Partida criada!")
+            print("Jogo: Valorant")
+            print("ID da partida:",ID)
+            print("Participantes:")
+            break
+
+    clientSocket.close()
