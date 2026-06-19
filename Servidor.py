fd4190fcb0df6a232eb8c0cf6ff2d67154147755
Jonathan Alexandre Rodrigues Alves <91800109+Jhon2910@users.noreply.github.com>
2026-06-19 10:29:17 -0300

    Update Servidor.py

--- Servidor.py
+++ Servidor.py
@@ -1,17 +1,18 @@
 from socket import *
 
-
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print('O servidor esta pronto esperando mensagens')
 
-Valorant = 0
-LOL = 0
-CS = 0
-FIFA = 0
-Minecraft = 0
+Fila = {
+    "Valorant": [],
+    "LOL": [],
+    "CS": [],
+    "FIFA": [],
+    "Minecraft": []
+}
 
 while True:
     connectionSocket, addr = serverSocket.accept()
@@ -25,13 +26,16 @@ while True:
     connectionSocket.send(nivel.encode())
 
     jogo = connectionSocket.recv(1024).decode()
-    print("jogo: ", jogo)
+    print("Jogo: ", jogo)
     connectionSocket.send(jogo.encode())
 
-
-    if jogo == "Valorant":
-        Valorant += 1
-        connectionSocket.send(str(Valorant).encode())
+    if jogo in Fila:
+        if nome in Fila[jogo]:
+            Fila[jogo].remove(nome)
+        connectionSocket.send("JaNaFila".encode())
+        else:
+            Fila[jogo].append(nome)
+            connectionSocket.send("JaNaFila".encode())
     elif jogo == "LOL":
         LOL += 1
         connectionSocket.send(str(LOL).encode())
@@ -44,5 +48,7 @@ while True:
     elif jogo == "Minecraft":
         Minecraft += 1
         connectionSocket.send(str(Minecraft).encode())
+    else:
+        print("Jogo inválido")
 
     connectionSocket.close()
