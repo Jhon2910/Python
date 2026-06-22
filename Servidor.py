f9c195211c837abc0c654a2a996197c234b44ee7
Jonathan Alexandre Rodrigues Alves <91800109+Jhon2910@users.noreply.github.com>
2026-06-22 08:34:31 -0300

    Update Servidor.py

--- Servidor.py
+++ Servidor.py
@@ -1,10 +1,9 @@
 from socket import *
-
 serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-serverSocket.bind(('', serverPort))
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
+
 print('O servidor esta pronto esperando mensagens')
 
 Fila = {
