c3525a59df89fa310ef56b72c59703cdb198661a
Jonathan Alexandre Rodrigues Alves <91800109+Jhon2910@users.noreply.github.com>
2026-06-23 10:22:28 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,4 +1,5 @@
 from socket import *
+
 serverName = 'localhost'
 serverPort = 12000
 
@@ -8,26 +9,26 @@ while True:
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName, serverPort))
 
-    nome = input('\nDigite seu nome: ')
-    print('Olá,', nome)
+    nome = input("\nDigite seu nome: ")
+    print("Olá,", nome)
     clientSocket.send(nome.encode())
-    clientSocket.recv(1024)
+    clientSocket.recv(1024)#Envia o nome para o servidor
 
-    nivel = input('Digite seu nível (1 a 100): ')
+    nivel = input("Digite seu nível (1 a 100): ")
     print("Seu nível é:", nivel)
     clientSocket.send(nivel.encode())
-    clientSocket.recv(1024)
+    clientSocket.recv(1024)#Envia o nivel para o servidor
 
     print("Jogos disponíveis:", jogosDisponiveis)
-    jogo = input('Qual jogo gostaria de jogar? ')
-    clientSocket.send(jogo.encode())
+    jogo = input("Qual jogo gostaria de jogar? ")
+    clientSocket.send(jogo.encode())#Envia o jogo desejado para o servidor
 
-    resposta = clientSocket.recv(1024).decode()
+    resposta = clientSocket.recv(1024).decode()#recebe a resposta do servidor
     clientSocket.close()
 
     print("\nResposta do servidor")
     print(resposta)
 
-    continuar = input('\nDeseja adicionar outro jogador (S/N)? ')
-    if continuar.upper() != 'S':
+    continuar = input("\nDeseja adicionar outro jogador (S/N)? ")
+    if continuar.upper() != "S":
         break
