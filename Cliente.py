1d5857994df7e447fbad6cd43735b153e38841e8
Jonathan Alexandre Rodrigues Alves <91800109+Jhon2910@users.noreply.github.com>
2026-06-23 09:28:19 -0300

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,67 +1,34 @@
 from socket import *
-import random
-import time
 
 serverName = 'localhost'
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
 
-
-listaDeJogos = {
-    "Valorant": 4,
-    "LOL": 5,
-    "CS": 4,
-    "FIFA": 2,
-    "Minecraft": 3
-}
+jogosDisponiveis = ["Valorant", "LOL", "CS", "FIFA", "Minecraft"]
 
 while True:
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName, serverPort))
 
     nome = input('\nDigite seu nome: ')
-    print('Olá', nome)
+    print('Olá,', nome)
     clientSocket.send(nome.encode())
     clientSocket.recv(1024)
 
-    nivel = input('Digite seu nivel (1 a 100): ')
-    print("Seu nivel é: ", nivel)
+    nivel = input('Digite seu nível (1 a 100): ')
+    print("Seu nível é:", nivel)
     clientSocket.send(nivel.encode())
     clientSocket.recv(1024)
 
-    print("Lista de jogos: ", listaDeJogos)
-    Jogo = input('Qual jogo gostaria de jogar? ')
-    clientSocket.send(Jogo.encode())
-    clientSocket.recv(1024)  # eco do jogo
+    print("Jogos disponíveis:", jogosDisponiveis)
+    jogo = input('Qual jogo gostaria de jogar? ')
+    clientSocket.send(jogo.encode())
 
-    resposta = clientSocket.recv(1024).decode()  # resposta final do servidor
+    resposta = clientSocket.recv(1024).decode()
     clientSocket.close()
 
+    print("\nResposta do servidor")
     print(resposta)
 
-    if Jogo in listaDeJogos:
-        jogadores = int(resposta.split(":")[-1].strip())
-        necessarios = listaDeJogos[Jogo] - jogadores
-
-        if necessarios > 0:
-            print("\nSão necessários mais", necessarios, "para jogar, você foi adicionado à fila de", Jogo)
-            time.sleep(2)
-        else:
-            ID = random.randint(1, 300)
-            print("\n------------------------------------------------")
-            print("Quantidade de jogadores alcançada!:", jogadores)
-            print("Partida criada!")
-            print("Jogo:", Jogo)
-            print("ID da partida:", ID)
-            print("Participantes:", jogadores)
-
-            continuar = input("Gostaria de continuar configurando os outros jogadores (S/N)? ")
-            if continuar == "S":
-                print("Continuando!")
-                time.sleep(1)
-            elif continuar == "N":
-                break
-    else:
-        print("Jogo não encontrado! Tente novamente.")
+    continuar = input('\nDeseja simular outro jogador (S/N)? ')
+    if continuar.upper() != 'S':
         break
