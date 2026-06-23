2f08b69a087d14e2f689e6ae660b048a8654a5d3
Jonathan Alexandre Rodrigues Alves <91800109+Jhon2910@users.noreply.github.com>
2026-06-23 09:50:19 -0300

    Update Servidor.py

--- Servidor.py
+++ Servidor.py
@@ -1,12 +1,15 @@
 from socket import *
+import random
+
 serverPort = 12000
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(('',serverPort))
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 
 print('O servidor esta pronto esperando mensagens')
 
-Fila = {
+filas = {
     "Valorant": [],
     "LOL": [],
     "CS": [],
@@ -14,33 +17,67 @@ Fila = {
     "Minecraft": []
 }
 
+jogadoresNecessarios = {
+    "Valorant": 4,
+    "LOL": 5,
+    "CS": 4,
+    "FIFA": 2,
+    "Minecraft": 3
+}
+
 while True:
     connectionSocket, addr = serverSocket.accept()
 
     nome = connectionSocket.recv(1024).decode()
-    print("Nome: ", nome)
-    connectionSocket.send(nome.encode())
+    print("Nome:", nome)
+    connectionSocket.send(b"OK")
 
     nivel = connectionSocket.recv(1024).decode()
-    print("Nível: ", nivel)
-    connectionSocket.send(nivel.encode())
+    print("Nível:", nivel)
+    connectionSocket.send(b"OK")
 
     jogo = connectionSocket.recv(1024).decode()
-    print("Jogo: ", jogo)
-    connectionSocket.send(jogo.encode())
+    print("Jogo:", jogo)
 
-    if jogo in Fila:
-        if nome in Fila[jogo]:
-            Fila[jogo].remove(nome)
-            resposta = f"Removido. Jogadores na fila: {len(Fila[jogo])}"
-        else:
-            Fila[jogo].append(nome)
-            resposta = f"Adicionado. Jogadores na fila: {len(Fila[jogo])}"
-
-        print(Fila)
+    if jogo not in filas:
+        resposta = "Jogo" + jogo + "não encontrado na lista de jogos disponíveis."
         connectionSocket.send(resposta.encode())
+        connectionSocket.close()
+        continue
+
+    filas[jogo].append(nome)
+    print("Fila atual de", jogo ,":" , filas[jogo])
+
+    necessarios = jogadoresNecessarios[jogo]
+
+    if len(filas[jogo]) >= necessarios:
+        participantes = filas[jogo][:necessarios]
+        filas[jogo] = filas[jogo][necessarios:]
+
+        idPartida = random.randint(1, 999)
+
+        print("\n------------------------------------------------")
+        print("Partida criada!")
+        print("Jogo:", jogo)
+        print("ID da partida:", idPartida)
+        print("Participantes:", participantes)
+
+
+        resposta = (
+            f"Uma nova partida foi criada.\n"
+            f"Jogo: { jogo }\n"
+            f"ID da partida: {idPartida}\n"
+            f"{len(participantes)} jogadores foram selecionados\n"
+            f"Participantes: {', '.join(participantes)}"
+        )
     else:
-        connectionSocket.send("Jogo inválido".encode())
+        posicao = len(filas[jogo])
+        resposta = (
+            f"Você foi adicionado à fila de espera.\n"
+            f"Jogo: {jogo}\n"
+            f"Posição na fila: {posicao}\n"
+            f"Faltam {necessarios - posicao} jogador para formar uma partida."
+        )
 
-
+    connectionSocket.send(resposta.encode())
     connectionSocket.close()
